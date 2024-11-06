90882c9df2bff6f1719ca9b9cd046c1f14987d8c
kyiaz <ashimalievkyaz@gmail.com>
2024-11-06 18:27:47 +0600

    update

--- src/app/(site)/bleach/shinigami/othershinigami/page.tsx
+++
@@ -1,6 +0,0 @@
-import OtherShinigami from "@/app/components/pages/BleachSection/Shinigami/OtherShinigami/OtherShinigami";
-import React from "react";
-
-const page = () => <OtherShinigami />;
-
-export default page;
